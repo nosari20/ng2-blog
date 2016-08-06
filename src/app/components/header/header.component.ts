20031f9a24767df3a3bf45f413ef498dc1552fba
ach0210s ach <ach0210s ach>
2016-08-06 23:32:50 +0200

    misc

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,18 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import {HeaderLinkComponent} from "./header-link/";
 
 @Component({
   moduleId: module.id,
   selector: 'app-header',
   templateUrl: 'header.component.html',
   styleUrls: ['header.component.css'],
-  host:     {'[class.md-primary]':'true', '[class.md-background]':'true'}
+  host:     {'[class.md-primary]':'true', '[class.md-background]':'true', '[class.push]':'_fixed', '(window:scroll)': 'track($event)'}
 })
 export class HeaderComponent implements OnInit {
-
+  private _fixed:boolean = false;
+  private _value:number = 65;
   constructor() {}
 
   ngOnInit() {
   }
+
+  toggleFixedBar(state:boolean){
+    this._fixed = state;
+  }
+
+  track(e) {
+    var doc = document, w = window;
+    var x, y, docEl;
+    if (this._value != 0) {
+      if (typeof w.pageYOffset === 'number') {
+        y = w.pageYOffset;
+      } else {
+        docEl = (doc.compatMode && doc.compatMode === 'CSS1Compat') ? doc.documentElement : doc.body;
+        y = docEl.scrollTop;
+      }
+      if ((y > this._value)) {
+        this._fixed = true;
+      } else {
+        this._fixed = false;
+      }
+    }
+  }
+
+
+
+
 
 }
